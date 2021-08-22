c578a168e435eaa0da278c4928fb89fa53bee210
jassemb <benalijassem201@gmail.com>
2021-08-21 20:04:16 -0700

    main function

--- main.c
+++ main.c
@@ -1,15 +1,37 @@
 #include "simpleshell.h"
 /**
- * main - Load config files, if any. Run command loop.
- * @argc: number of arguemnets.
- * @argv: number of arguemnets.
+ * main - creates a prompt that reads input, sparses it, executes and waits
+ * for another command unless told to exit
+ * @ac: number of arguemnets
+ * @av: array of arguements
+ * @env: environment variable
  * Return: EXIT_SUCCESS
  */
-
-int main(int argc, char **argv)
+int main(int ac __attribute__((unused)), char **av, char **env)
 {
+	char *line;
+	char **args, **path;
+	int count = 0, status = 0;
+	(void) av;
+	signal(SIGINT, handle_signal);
+	while (1)
+	{
+		prompt();
+		line = read_input();
+		args = sparse_str(line, env);
 
-	lsh_loop();
-
+		if ((_strcmp(args[0], "\n") != 0) && (_strcmp(args[0], "env") != 0))
+		{
+			count += 1;
+			path = search_path(env); 
+			status = _stat(args, path);
+			child_process(av, args, env, status, count);
+		}
+		else
+		{
+			free(args);
+		}
+		free(line);
+	}
 	return (EXIT_SUCCESS);
 }
